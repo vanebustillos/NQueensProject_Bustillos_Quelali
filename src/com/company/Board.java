d5a5bb8e96223b8e964ad9a8eddbee320b9d9fac
AlejandraDay <alejandra.day.123@gmail.com>
2020-08-16 00:09:23 -0400

    structuring solution

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,21 +1,35 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Board {
 
-    int [][] board;
+    char [][] board;
     int size;
+    //boolean [] vertical;
+    //boolean [] horizontal;
+    //boolean [] diagonalInf;
+    //boolean [] diagonalSup;
+    //char queen =
+    List<Coord> notAvailablePositions;
+    List<Coord> queens;
     public Board(int size){
-         board = new int [size][size];
+         board = new char [size][size];
          this.size = size;
      }
 
      public void initBoard(){
+        Random random = new Random();
+        int initial = random.nextInt(size-1);
         if (size >= 4){
             for(int i = 0; i < size; i++){
                 for(int j= 0; j < size; j++){
-                    board[i][j] = 0;
+                    board[i][j] = ' ';
                 }
             }
+            board[initial][initial] = 'Q'; // board[column][file]
             printBoard();
         }
          else{
@@ -33,4 +47,25 @@ public class Board {
          }
          System.out.println("+-----".repeat(size) + "+" );
      }
+    private void searchAvailablePositions() {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; i < size; j++) {
+               if( board[i][j] == ' ') {
+
+               }
+            }
+        }
+    }
+    private void searchSolution() {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; i < size; j++) {
+            }
+        }
+
+     }
+
+     private void saveNotAvailablePosition(Coord queen) {
+
+     }
+
 }
---
+++ src/com/company/Coord.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Coord {
+        int row;
+        int column;
+    public Coord(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coord)) return false;
+        Coord queen = (Coord) o;
+        return row == queen.row &&
+                column == queen.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+
+
+}
